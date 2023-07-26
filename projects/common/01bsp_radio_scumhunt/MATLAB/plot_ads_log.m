f5f49976972edd53b0ea4ed66d6c689064423c7e
Daniel Lovell <lovell.daniel92@gmail.com>
2023-07-26 15:08:01 -0700

    Color by RSSI, time reconstruction

--- projects/common/01bsp_radio_scumhunt/MATLAB/plot_ads_log.m
+++ projects/common/01bsp_radio_scumhunt/MATLAB/plot_ads_log.m
@@ -1,10 +1,11 @@
 % Save the data
-save('data.mat', 'data_struct');
+
 
 % Close the serial port
 if(exist('ser', 'var'))
     ser.delete();
     clear ser;
+    save('data.mat', 'data_struct');
 end
 % Load the data
 load('data.mat');
@@ -14,14 +15,31 @@ num_data_points = length(data_struct);
 
 data = [];
 lqi = [];
+rssi = [];
+t = [];
+sps = 250;
+tvec = 0:1/sps:(0+7/sps);
 % Extract the time, data, and LQI from the structure
 for i = 2:num_data_points
     data = [data, data_struct(i).data(:)'];
     lqi = [lqi, repmat(data_struct(i).lqi, 1, 8)];
+    rssi = [rssi, repmat(data_struct(i).rssi, 1, 8)];
+    t = [t, tvec + (data_struct(i).counter_val / 32.768e3)];
 end
 
+
+
 % Normalize LQI to the range [0, 1]
-lqi = lqi / 255;
+idx = lqi > 0;
+lqi(idx) = [];
+data(idx) = [];
+rssi(idx) = [];
+t(idx) = [];
+median(data);
+
+figure;
+plot(t, data, '.-');
+
 
 % Create a color map that goes from red to green
 cmap = [linspace(1, 0, 256)', linspace(0, 1, 256)', zeros(256, 1)];
@@ -29,13 +47,19 @@ cmap = [linspace(1, 0, 256)', linspace(0, 1, 256)', zeros(256, 1)];
 % Create a new figure
 figure;
 
+rssi_norm = (rssi - min(rssi));
+rssi_norm = round(rssi_norm * (255 / max(rssi_norm)));
+
 % Plot the data
-cmap(round(lqi*255)+1, :);
-scatter(1:length(data), data, 10, cmap(round(lqi*255)+1, :), 'filled');
+plot(t, data, 'k-');
 hold on;
+scatter(t, data, 10, cmap(rssi_norm+1, :), 'filled');
+
 
 % Set the color map
 colormap(cmap);
+
+
 
 % Add a color bar
 colorbar;
@@ -43,7 +67,7 @@ colorbar;
 % Set the labels
 xlabel('Time (s)');
 ylabel('Data');
-title('Data vs. Time colored by LQI');
+title('Data vs. Time colored by RSSI');
 
 % Hold off the plot
 hold off;
